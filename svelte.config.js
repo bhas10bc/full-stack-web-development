0a0b2f46dcbd8321fc3011ac85669f911a359795
bhas10bc <94827040+bhas10bc@users.noreply.github.com>
2022-01-04 15:57:27 +0530

    Revert "Add Gitpod configuration"

--- svelte.config.js
+++ svelte.config.js
@@ -11,15 +11,7 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			server:{
-				hmr:{
-					clientPort: process.env.HMR_HOST? 443: 3000,
-					host: process.env.HMR_HOST? process.env.HMR_HOST.substring("https://".length): "localhost"
-				}
-			} 
-		}
+		target: '#svelte'
 	}
 };
 
